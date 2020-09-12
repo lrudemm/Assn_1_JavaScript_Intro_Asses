080650bbe1aaceae303b95282a6fb5fa2146d87c
Larry R <69208186+lrudemm@users.noreply.github.com>
2020-09-12 18:26:49 -0400

    removed practice code and begin working on code for assignment

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const x = 7;
+var firstName = "Larry";
+var lastName = "Rud";
+var age = "47";
+console.log()
+
+let num = Number(prompt("What is your age?"));
+
+if (num < 21) {
+  console.log("It is illegal for me to drink in the states.");
+} else if (num > 21) {
+  console.log("I am legally able to drink in the states.");
+} else {
+  console.log("I am just barely able to drink legally in the states.");
+}
