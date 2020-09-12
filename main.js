97fb2365956c7569cf05f50729aca409af6413b0
Larry R <69208186+lrudemm@users.noreply.github.com>
2020-09-12 19:14:39 -0400

    final code ready for submission to canvas

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ console.log("My name is "+ firstName + ' ' + lastName);
 //fire an alert with the words "I am AGE HERE"//
 console.log("I am " + age + " years old.");
 
-if (num < 21) {
+if (age < 21) {
   console.log("It is illegal for me to drink in the states.");
-} else if (num > 21) {
+} else if (age > 21) {
   console.log("I am legally able to drink in the states.");
 } else {
   console.log("I am just barely able to drink legally in the states.");
