c8c9609bd6dd148df3134c6adf825b70f568097b
Larry R <69208186+lrudemm@users.noreply.github.com>
2020-09-12 20:47:57 -0400

    changed var to const and changed console.log to alert

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // First add variables for first name, last name and age//
-var firstName = "Larry";
-var lastName = "Rud";
-var age = "47";
+const firstName = "Larry";
+const lastName = "Rud";
+const age = "47";
 //fire an alert with words "My name is FULL NAME HERE"//
-console.log("My name is "+ firstName + ' ' + lastName);
+alert("My name is "+ firstName + ' ' + lastName);
 //fire an alert with the words "I am AGE HERE"//
-console.log("I am " + age + " years old.");
+alert("I am " + age + " years old.");
 
 if (age < 21) {
-  console.log("It is illegal for me to drink in the states.");
+  alert("It is illegal for me to drink in the states.");
 } else if (age > 21) {
-  console.log("I am legally able to drink in the states.");
+  alert("I am legally able to drink in the states.");
 } else {
-  console.log("I am just barely able to drink legally in the states.");
+  alert("I am just barely able to drink legally in the states.");
 }
 console.log("This is a very challenging class and I am having fun learning to code!");
